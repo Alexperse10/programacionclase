00ea7181aa78e692dc2a5e18d410f5d752639552
AlexPerse10 <spuknitfire@gmail.com>
2025-09-29 23:58:23 +0200

    ejercicio 8 terminado con if else

--- 1evbateriaejerciciossencillos/1evbateriaejerciciossencillos/src/main/java/org/example/ejercicio8.java
+++ 1evbateriaejerciciossencillos/1evbateriaejerciciossencillos/src/main/java/org/example/ejercicio8.java
@@ -1,5 +1,26 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class ejercicio8 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("introduce un lado a");
+        double ladoa = sc.nextDouble();
+        System.out.println("introduce un lado b");
+        double ladob = sc.nextDouble();
+        System.out.println("introduce un lado c");
+        double ladoc = sc.nextDouble();
+
+        if (ladoa == ladob && ladob == ladoc){
+            System.out.println("el triangulo es equilatero");
+        } else if (ladoa == ladoc || ladoa == ladob) {
+            System.out.println("el triangulo es isosceles");
+
+        } else {
+            System.out.println("el triangulo es escaleno");
+
+        }
+    }
+
 }

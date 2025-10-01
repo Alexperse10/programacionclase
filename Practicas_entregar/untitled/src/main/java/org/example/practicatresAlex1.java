c08bbc29d51475039f947fabef111679c2524ee0
AlexPerse10 <spuknitfire@gmail.com>
2025-10-01 23:49:06 +0200

    he introducido un segundo bucle do-while para controlar el peso<0 que repita la preguntar de introducir peso

--- Practicas_entregar/untitled/src/main/java/org/example/practicatresAlex1.java
+++ Practicas_entregar/untitled/src/main/java/org/example/practicatresAlex1.java
@@ -35,27 +35,30 @@ public class practicatresAlex1 {
                 System.out.println("altura correcta");
             }
         } while (altura > alturamaxconst || altura <= 0 || altura < alturaminconst);
+        do {
+            try {
+                System.out.println("introduce tu peso");
+                peso = sc.nextDouble();
+            } catch (InputMismatchException err) {
+                System.out.println("introduce un valor valido");
+                return;
+            }
+            System.out.println("Leyendo peso....");
+            pesominimo = (altura * 2 / 8);
+            if (peso < 0) {
+                System.out.println("introduce un valor positivo");
+            } else if (peso < pesominimo) {
+                totalpeso = pesominimo - peso;
+                System.out.println("Lo siento, no superas el peso minimo, te faltan " + totalpeso + " kg para subir a la atraccion");
+            } else if (peso > pesomaxconst) {
+                pesomax = peso - pesomaxconst;
+                System.out.println("Lo siento, sobrepasas en " + pesomax + " kg el limite de la atraccion");
 
-        try {
-            System.out.println("introduce tu peso");
-            peso = sc.nextDouble();
-        } catch (InputMismatchException err) {
-            System.out.println("introduce un valor valido");
-            return;
-        }
-        System.out.println("Leyendo peso....");
-        pesominimo = (altura * 2 / 8);
-        if (peso < pesominimo) {
-            totalpeso = pesominimo - peso;
-            System.out.println("Lo siento, no superas el peso minimo, te faltan " + totalpeso + " kg para subir a la atraccion");
-        } else if (peso > pesomaxconst) {
-            pesomax = peso - pesomaxconst;
-            System.out.println("Lo siento, sobrepasas en " + pesomax + " kg el limite de la atraccion");
-
-        } else {
-            System.out.println("pesas " + peso + " kg, peso minimo calculado: " + pesominimo);
-            System.out.println("¡¡Sube a la atraccion!!");
-        }
+            } else {
+                System.out.println("pesas " + peso + " kg, peso minimo calculado: " + pesominimo);
+                System.out.println("¡¡Sube a la atraccion!!");
+            }
+        } while (peso < 0);
     }
 }
 
